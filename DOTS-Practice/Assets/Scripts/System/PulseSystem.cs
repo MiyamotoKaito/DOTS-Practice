7c1d9f4a0bf4c5f5d982082591da72fd3a89aea5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:04:32 +0900

    [add]WalkerとDancerを持ってるEntityに作用するシステム

---
+++ DOTS-Practice/Assets/Scripts/System/PulseSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+/// <summary>
+/// DancerとWalkerとLocalTransformを持っているEntityに処理を行うシステム
+/// </summary>
+public partial struct PulseSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        var elapsed = (float)SystemAPI.Time.ElapsedTime;
+        foreach (var (dancer, walker, xform) in
+            SystemAPI.Query<RefRO<Dancer>,
+            RefRO<Walker>,
+            RefRW<LocalTransform>>())
+        {
+            var t = dancer.ValueRO.Speed * elapsed;
+
+            //経過時間によってスケールを変える
+            xform.ValueRW.Scale = 1.1f - 0.3f * math.abs(math.cos(t));
+        }
+    }
+}
