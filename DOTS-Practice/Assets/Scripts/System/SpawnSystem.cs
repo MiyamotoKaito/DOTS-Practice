049969a0c6dd9a3552a6c9cb08d9fae2abbc0339
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:49:35 +0900

    [add]オブジェクト生成用のシステム

---
+++ DOTS-Practice/Assets/Scripts/System/SpawnSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+public partial struct SpawnSystem : ISystem
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
