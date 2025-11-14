23e1af29d28706e0b2d9c7fbe4591a50dfd7075c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 14:26:08 +0900

    [add]フィールド変数追加

--- DOTS-Practice/Assets/Scenes/Scripts/ComponentData/WalkerAuthoring.cs
+++ DOTS-Practice/Assets/Scenes/Scripts/ComponentData/WalkerAuthoring.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class WalkerAuthoring : MonoBehaviour
 {
-    
+    [SerializeField, Header("前方向に動かす速度")]
+    private float _forwardSpeed;
+    [SerializeField, Header("角度を変える速度")]
+    private float _angularSpeed;
 }
 public struct Walker : IComponentData
 {
