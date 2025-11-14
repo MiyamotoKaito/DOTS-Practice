45eeb158894337c9ca9fb3e66d526bcee325aaf0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:02:58 +0900

    [add]歩かせるシステム

---
+++ DOTS-Practice/Assets/Scenes/Scripts/System/WalkerSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+/// <summary>
+/// 円を描くように動かすWalker用のシステム
+/// </summary>
+public partial struct WalkerSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        var dt = SystemAPI.Time.DeltaTime;
+
+        foreach (var (walker, xform) in
+            SystemAPI.Query<RefRO<Walker>,
+            RefRW<LocalTransform>>())
+        {
+            var rot = quaternion.RotateY(walker.ValueRO.AngularSpeed * dt);
+            var fwd = math.mul(rot, xform.ValueRO.Forward());
+
+            xform.ValueRW.Position += fwd * walker.ValueRO.ForwardSpeed * dt;
+            xform.ValueRW.Rotation = quaternion.LookRotation(fwd, xform.ValueRO.Up());
+        }
+    }
+}
