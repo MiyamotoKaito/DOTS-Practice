d25763d3c2c25c89f48ad7430d7c5e4c74117b31
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:52:34 +0900

    [add]Summary追加

--- DOTS-Practice/Assets/Scripts/System/SpawnSystem.cs
+++ DOTS-Practice/Assets/Scripts/System/SpawnSystem.cs
@@ -1,7 +1,14 @@
 ﻿using Unity.Entities;
 
-
+/// <summary>
+/// アップデートのタイミングを明示的に呼び出し
+/// 他のUpdateよりも先に実行したいから
+/// </summary>
 [UpdateInGroup(typeof(InitializationSystemGroup))]
+
+/// <summary>
+/// オブジェクトを生成するシステム
+/// </summary>
 public partial struct SpawnSystem : ISystem
 {
 
