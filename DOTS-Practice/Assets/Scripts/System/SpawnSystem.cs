19c4b6a85419560bfcaebf12000d7d620dd8be16
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:50:12 +0900

    [add]属性付与でアップデートのタイミングを明示的に呼び出し

--- DOTS-Practice/Assets/Scripts/System/SpawnSystem.cs
+++ DOTS-Practice/Assets/Scripts/System/SpawnSystem.cs
@@ -1,16 +1,8 @@
 ﻿using Unity.Entities;
 
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
 public partial struct SpawnSystem : ISystem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
