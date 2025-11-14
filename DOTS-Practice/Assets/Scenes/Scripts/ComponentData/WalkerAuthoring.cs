0672641bb1dbf1f0833114483a0bbf3021ff8308
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 14:26:40 +0900

    [add]値を構造体に変換する処理

--- DOTS-Practice/Assets/Scenes/Scripts/ComponentData/WalkerAuthoring.cs
+++ DOTS-Practice/Assets/Scenes/Scripts/ComponentData/WalkerAuthoring.cs
@@ -7,6 +7,19 @@ public class WalkerAuthoring : MonoBehaviour
     private float _forwardSpeed;
     [SerializeField, Header("角度を変える速度")]
     private float _angularSpeed;
+
+    class Baker : Baker<WalkerAuthoring>
+    {
+        public override void Bake(WalkerAuthoring authoring)
+        {
+            var data = new Walker()
+            {
+                ForwardSpeed = authoring._forwardSpeed,
+                AngularSpeed = authoring._angularSpeed
+            };
+            AddComponent(GetEntity(TransformUsageFlags.Dynamic), data);
+        }
+    }
 }
 public struct Walker : IComponentData
 {
