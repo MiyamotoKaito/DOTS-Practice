666b9e8f5077216c40a077ff6ccc290b4bdd35d7
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-11-14 17:44:12 +0900

    [add]生成用のスクリプト

---
+++ DOTS-Practice/Assets/Scripts/ComponentData/ConfigAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class ConfigAuthoring : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _character;
+    [SerializeField]
+    private float _spawnRadius;
+    [SerializeField]
+    private int _spawnCount;
+    [SerializeField]
+    private int _randomSeed;
+}
+public struct Config : IComponentData
+{
+    public Entity Character;
+    public float SpawnRadius;
+    public int SpawnCount;
+    public int RandomSeed;
+}
